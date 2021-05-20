1238a44a753c2c6828e3dba316620a48b5702d36
binaryExDev <binarydev51@gmail.com>
2021-05-20 09:53:32 -0400

    Fix search result

--- src/pages/LibrarySearch.vue
+++ src/pages/LibrarySearch.vue
@@ -36,9 +36,9 @@
     </div>
 
     <div class="mt-0">
-      <h3 class="mb-4 ml-8" v-if="!currentApplets.length">
+      <h4 class="mb-4 ml-8" v-if="!currentApplets.length">
         No results found
-      </h3>
+      </h4>
       <v-card
         v-else
         class="mx-auto mb-4 d-flex pa-md-2"
