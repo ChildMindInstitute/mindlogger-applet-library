5e161958597e4e847db68e7038eb25391a277446
jDev <rjtech0018@gmail.com>
2022-01-28 08:43:39 -0500

    fix searching behavior applets

--- src/pages/LibrarySearch.vue
+++ src/pages/LibrarySearch.vue
@@ -127,7 +127,7 @@
                   <markdown :source="highlight(getItemtitle(item.title), true)"></markdown>
                   <template v-if="leaf">
                     <div v-show="item.selected">
-                      <div v-if="item.inputType === 'radio' || item.inputType === 'checkbox'">
+                      <div v-if="item.inputType == 'radio' || item.inputType == 'checkbox' || item.inputType == 'pastBehaviorTracker' || item.inputType == 'futureBehaviorTracker'">
                         <div
                           v-for="option in item.options"
                           :key="option.name"
